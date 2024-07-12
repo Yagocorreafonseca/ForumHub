aae74b3ec0b43dd975acdc0f3f6a1b42fba86361
Yagocorreafonseca <yago_correa@hotmail.com>
2024-07-12 16:59:15 -0300

    Atualização link github

--- src/main/java/com/alura/ForumHub/ForumHub/Springdoc/SpringDocConfigurations.java
+++ src/main/java/com/alura/ForumHub/ForumHub/Springdoc/SpringDocConfigurations.java
@@ -17,7 +17,7 @@ public class SpringDocConfigurations {
                         .title("ForumHub")
                         .version("1.0")
                         .description("O FórumHub é uma API REST desenvolvida utilizando Spring Boot, simulando o back code de um fórum de discussão. Nesta API, é possível criar, visualizar, atualizar e deletar tópicos de discussão, perfis, respostas, cursos e usuários. Apenas usuários autenticados podem acessar os tópicos. ")
-                        .termsOfService("")
+                        .termsOfService("https://github.com/Yagocorreafonseca/ForumHub")
                         .contact(new Contact()
                                 .name("Yago Correa Fonseca")
                                 .url("https://github.com/Yagocorreafonseca")))
